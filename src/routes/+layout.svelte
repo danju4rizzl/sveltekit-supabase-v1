b15423fcd08fbf776d3bcbb5d597266c15c431c4
danju4rizzl <danjumashiwaju@gmail.com>
2024-06-29 21:43:37 +0200

    🚀initial commit + tailwind + daisyUI + supabase

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import "../app.css";
+
+/**
+ * TODO: Remove the greetUser function  and clean the page
+ */
+const d = new  Date()
+
+const greetUser = (time: number) => `Good ${time < 12 ? 'Morning' : time < 18 ? 'Afternoon' : 'Evening'}, Dev's 👋`;
+
+const currentHour = d.getHours()
+
+</script>
+
+<!-- Navbar -->
+
+<div class="p-5 text-center">
+  <h1 class="text-xl ">{greetUser(currentHour)}</h1>
+</div>
+
+<slot></slot>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+
+<div class="py-40 px-10 text-center gap-5 grid">
+<h3 class="text-3xl">Svelte + DaisyUI + Supabase</h3>
+
+<p class="text-sm">Like what you see, click the button to visit my blog</p>
+  <a class="btn btn-primary" href="https://deejaydev.work" target="_blank">Visit my Blog for More</a>
+</div>
